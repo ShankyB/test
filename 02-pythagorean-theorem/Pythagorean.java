0d15633f37db53de24ef6c05a11c84f74aecc2bb
Shanky B <shankybakshi@gmail.com>
2020-10-01 20:42:03 -0500

    second project

---
+++ 02-pythagorean-theorem/Pythagorean.java
@@ -0,0 +1,8 @@
+import java.lang.Math;
+
+public class Pythagorean {
+    public double getHypo (int legA, int legB) {
+        double squareC= legA*legA + legB*legB;
+        double squareRoot = Math.sqrt(squareC); // 2.0
+    }
+}
---
+++ 02-pythagorean-theorem/PythagoreanTest.java
@@ -0,0 +1,7 @@
+public class PythagoreanTest {
+    public static void main(String[] args) {
+        Pythagorean hypoTest= new Pythagorean();
+            double myGethypo= hypoTest.getHypo();
+            System.out.println(myGethypo(2,3));
+    }
+}
